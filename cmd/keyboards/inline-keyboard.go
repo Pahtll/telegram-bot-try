e8716b33d21fbaa0c9d4dc887ea3b48ac2334653
Pahtll <astahov.egor008@gmail.com>
2024-05-05 10:13:54 +1000

    commented xD

--- cmd/keyboards/inline-keyboard.go
+++ cmd/keyboards/inline-keyboard.go
@@ -1,16 +1,13 @@
 package keyboards
 
-import (
-	tu "github.com/mymmrac/telego/telegoutil"
-)
+// import (
+// 	tu "github.com/mymmrac/telego/telegoutil"
+// )
 
-func ActivateInlineKeyboard() {
-	keyboard := tu.Keyboard(
-		tu.KeyboardRow(
-			tu.KeyboardButton("Start to learn Go!").
-				WithRequestContact(true).WithWebApp("https://go.dev"),
-			tu.KeyboardButton("Start to learn Python!").
-				WithRequestContact(true).WithWebApp("https://python.org"),
-		),
-	)
-}
+// func ActivateInlineKeyboard() {
+// 	keyboard := tu.Keyboard(
+// 		tu.KeyboardRow(
+// 			tu.KeyboardButton("Start to learn Go!").WithWebApp()
+// 		)
+// 	)
+// }
